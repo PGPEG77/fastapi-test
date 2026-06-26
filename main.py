81780917fa2d0bb0cd8d7bcfffc32324b140c492
PGPEG77 <p.granqvist@live.se>
2026-06-27 00:25:50 +0200

    Add debug print for DATABASE_URL

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ app = FastAPI()
 # DATABASE SETUP
 # -----------------------------
 DATABASE_URL = os.getenv("DATABASE_URL")
+print("DEBUG_DATABASE_URL:", DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)

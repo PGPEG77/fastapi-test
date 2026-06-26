a3936e2c733a078a1783c4b04579e68428174818
PGPEG77 <p.granqvist@live.se>
2026-06-26 23:52:48 +0200

    Move backend to project root

--- Caller.py
+++
@@ -1,4 +0,0 @@
-import ValueTest1
-
-print("Nu anropar vi main() i ValueTest1:")
-ValueTest1.main()
--- Caller3.py
+++
@@ -1,5 +0,0 @@
-import ValueTest3
-
-print("Nu anropar vi main() i ValueTest3:")
-ValueTest3dir
-.main()
